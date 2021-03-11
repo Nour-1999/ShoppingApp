63ac925f6abdadf9434c1ce20569dabb6024f1a9
ASUS <noor.kh.mansour@gmail.com>
2021-03-11 17:47:27 +0200

    main update

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
 
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.deepPurple,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         routes: {
